3d7894e1460a9b8b9763d7b14abd06241b53bf85
Labboro <gtorrealba12@gmail.com>
2022-04-04 11:09:45 -0400

    Add div with children and apply styling

--- practice.js
+++ practice.js
@@ -1,6 +1,6 @@
 // const container = document.querySelector('#container');
 
-// const content = document.createElement('div1');
+// const content = document.createElement('div');
 // content.classList.add('content');
 // content.textContent = 'This is the glorious text-content!';
 
@@ -16,8 +16,14 @@ h3.textContent = "I'm a blue h3!"
 h3.style.cssText = 'color: blue'
 container.appendChild(h3)
 
-const div1 = document.querySelector('#thediv1')
-div1.setAttribute('style', 'background: pink')
-div1.textContent = 'Test'
-container.appendChild(div1)
+const div = document.querySelector('#thediv')
+div.setAttribute('style', 'background: pink; border: thin solid black')
+div.textContent = 'Test'
+const h1 = document.createElement('h1')
+h1.textContent = "I'm in a div"
+const pp = document.createElement('p')
+pp.textContent = "ME TOO!"
+thediv.appendChild(h1)
+thediv.appendChild(pp)
+container.appendChild(div)
 
